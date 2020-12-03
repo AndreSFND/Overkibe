9e0dfa40ddb97b8251e86f72acb1b35e61c963f7
André Santana <andre_sfnd@usp.br>
2020-12-03 15:51:35 -0300

    Initial version

---
+++ Assets/Game/Scripts/OrtographicSize.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrtographicSize : MonoBehaviour
+{
+
+    public SpriteRenderer background = null;
+
+    // Start is called before the first frame update
+    void Start() {
+
+        float screenRatio = (float) Screen.width / (float) Screen.height;
+        float targetRatio = background.bounds.size.x /  background.bounds.size.y;
+
+        if( screenRatio >= targetRatio ) {
+
+            Camera.main.orthographicSize = background.bounds.size.y / 2;
+
+        } else {
+
+            float differenceInSize = targetRatio / screenRatio;
+            Camera.main.orthographicSize = background.bounds.size.y / 2 * differenceInSize;
+
+        }
+
+    }
+
+}
---
+++ Assets/Game/Scripts/Player.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    private Rigidbody2D rb = null;
+    private bool onTable = false;
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        rb = GetComponent<Rigidbody2D>();
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+        float horizontalInput   = Input.GetAxis("Horizontal");
+        float verticalInput     = Input.GetAxis("Vertical");
+
+        rb.velocity = new Vector2(horizontalInput*speed, verticalInput*speed);
+
+        if( Input.GetKeyDown("space") && onTable ) {
+
+            Debug.Log("Interagindo com balcao");
+
+        }
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+
+        if( col.name == "MesaRange" ) {
+
+            Debug.Log("Aperte X para interagir");
+            onTable = true;
+
+        }
+
+    }
+
+    void OnTriggerExit2D(Collider2D col)
+    {
+
+        if( col.name == "MesaRange" ) {
+
+            Debug.Log("Não está mais interagindo");
+            onTable = false;
+
+        }
+
+    }
+    
+}
